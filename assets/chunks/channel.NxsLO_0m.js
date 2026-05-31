e5fe6537973e685f9e15ae8bc54ed75f3e36233d
Andressa Celant <andressa.celant@desbravador.com.br>
2026-05-31 16:55:33 -0300

    Updates

---
+++ assets/chunks/channel.NxsLO_0m.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.DmPK9EVH.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- assets/chunks/channel.te7MA-r4.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.StwBXveF.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
